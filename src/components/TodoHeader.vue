93ddbaa28cddb42ee8456120972e8b30c51c2fb0
YeWeon <yeone711@gmail.com>
2023-06-30 13:17:51 +0900

    feat: build env

--- src/components/TodoHeader.vue
+++ src/components/TodoHeader.vue
@@ -1,25 +1,24 @@
 <template>
-    <header>
-        <h1>TODO it!</h1>
-    </header>
+  <header>
+    <h1>TODO it!</h1>
+  </header>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 
 export default defineComponent({
-    setup () {
-        
-
-        return {}
-    }
-})
+  setup() {
+    const mode = process.env.VUE_APP_TITLE;
+    return { mode };
+  },
+});
 </script>
 
 <style scoped>
-    h1 {
-        color:#2F3852;
-        font-weight: 900;
-        margin: 2.5rem 0 1.5rem;
-    }
+h1 {
+  color: #2f3852;
+  font-weight: 900;
+  margin: 2.5rem 0 1.5rem;
+}
 </style>
